c756ba1f1a041f34cadc3d590bbe93d0080a7184
xuanhuang <xuanhuangwendao@gmail.com>
2022-04-30 11:34:41 +0800

    登录界面优化

--- lib/page/login_page.dart
+++ lib/page/login_page.dart
@@ -22,6 +22,9 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   var username;
   var password;
+  List<String> _radioList = [];
+  String _radioCheck = '普通用户';
+  int userType = 1;
 
   @override
   void initState() {
@@ -30,6 +33,9 @@ class _LoginPageState extends State<LoginPage> {
     username.addListener(() {});
     password = TextEditingController();
     password.addListener(() {});
+    _radioList.add("普通用户");
+    _radioList.add("团长");
+    _radioList.add("供应商");
   }
 
   @override
@@ -37,35 +43,37 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       body: Container(
         child: SingleChildScrollView(
+          padding: EdgeInsets.all(30),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               const SizedBox(
-                height: 120,
+                height: 70,
               ),
-              Row(mainAxisAlignment: MainAxisAlignment.center, children: const <Widget>[
-                Text(
-                  '社区团购',
-                  textAlign: TextAlign.right,
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.black45,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ]),
-              Row(mainAxisAlignment: MainAxisAlignment.center, children: const <Widget>[
-                Text(
-                  '登录',
-                  textAlign: TextAlign.right,
-                  style: TextStyle(
-                    fontSize: 25,
-                    height: 1.5,
-                    color: Colors.black45,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ]),
-              const SizedBox(height: 120),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const <Widget>[
+                    Image(
+                      image: AssetImage("assets/image/logo.jpg"),
+                      height: 200,
+                      width: 200,
+                    )
+                  ]),
+              // Row(
+              //     mainAxisAlignment: MainAxisAlignment.center,
+              //     children: const <Widget>[
+              //       Text(
+              //         '登录',
+              //         textAlign: TextAlign.right,
+              //         style: TextStyle(
+              //           fontSize: 25,
+              //           height: 1.5,
+              //           color: Colors.black45,
+              //           fontWeight: FontWeight.bold,
+              //         ),
+              //       ),
+              //     ]),
+              const SizedBox(height: 40),
               SizedBox(
                 child: Container(
                   padding: EdgeInsets.fromLTRB(20, 2, 8, 2),
@@ -106,163 +114,125 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                 ),
               ),
+              ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: _radioList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      title: Text(_radioList[index]),
+                      leading: Radio(
+                        value: _radioList[index],
+                        groupValue: _radioCheck,
+                        onChanged: (String? value) {
+                          setState(() {
+                            _radioCheck = value!;
+                          });
+                          userType = index + 1;
+                        },
+                      ),
+                      onTap:(){
+                        setState(() {
+                          _radioCheck = _radioList[index];
+                          userType = index + 1;
+                        });
+                      },
+                    );
+                  }),
               const SizedBox(
                 height: 30,
               ),
               SizedBox(
                   child: InkWell(
-                    child: Container(
-                        padding: EdgeInsets.fromLTRB(2, 15, 2, 15),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: Colors.deepOrangeAccent,
-                        ),
-                        alignment: Alignment.center,
-                        child: const Text(
-                          '用户登录',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              letterSpacing: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white70),
-                        )),
-                    onTap: () {
-                      print(username.text);
-                      print(password.text);
+                child: Container(
+                    padding: EdgeInsets.fromLTRB(2, 15, 2, 15),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.deepOrangeAccent,
+                    ),
+                    alignment: Alignment.center,
+                    child: const Text(
+                      '登录',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          letterSpacing: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white70),
+                    )),
+                onTap: () {
+                  print(username.text);
+                  print(password.text);
 
-                      Map<String, dynamic> params = {
-                        "username": username.text,
-                        "password": password.text,
-                        "userType": 1
-                      };
-                      NetRequest()
-                          .request(MyApi.LOGIN, params: params)
-                          .then((response) async {
-                        if (response.code == 200) {
-                          LoginResponse model = LoginResponse.fromJson(response.model);
-                          final prefs = await SharedPreferences.getInstance();
-                          await prefs.setString('token', model.token!);
-                          await prefs.setString('userType', "1");
+                  Map<String, dynamic> params = {
+                    "username": username.text,
+                    "password": password.text,
+                    "userType": userType
+                  };
+                  NetRequest()
+                      .request(MyApi.LOGIN, params: params)
+                      .then((response) async {
+                    if (response.code == 200) {
+                      LoginResponse model =
+                          LoginResponse.fromJson(response.model);
+                      final prefs = await SharedPreferences.getInstance();
+                      await prefs.setString('token', model.token!);
+                      await prefs.setString('userType', "1");
 
-
-                          Navigator.of(context).pushAndRemoveUntil(
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      ChangeNotifierProvider<BottomNaviProvider>(
-                                        create: (context) {
-                                          BottomNaviProvider provider =
+                      Navigator.of(context).pushAndRemoveUntil(
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  ChangeNotifierProvider<BottomNaviProvider>(
+                                    create: (context) {
+                                      BottomNaviProvider provider =
                                           BottomNaviProvider();
-                                          provider.changeBottomNaviIndex(0);
-                                          return provider;
-                                        },
-                                        child: Consumer<BottomNaviProvider>(
-                                            builder: (_, provider, __) {
-                                              return Container(child: IndexPage(userType: 1,));
-                                            }),
-                                      )),
-                                  (route) => false);
-                        } else {
-                          showAlertDialog(context, "登录失败", "");
-                        }
-                      }).catchError((error) {
-                        print(error);
-                        showAlertDialog(context, "", "error");
-                      });
-                    },
-                  )),
+                                      provider.changeBottomNaviIndex(0);
+                                      return provider;
+                                    },
+                                    child: Consumer<BottomNaviProvider>(
+                                        builder: (_, provider, __) {
+                                      return Container(
+                                          child: IndexPage(
+                                        userType: userType,
+                                      ));
+                                    }),
+                                  )),
+                          (route) => false);
+                    } else {
+                      showAlertDialog(context, "登录失败", "");
+                    }
+                  }).catchError((error) {
+                    print(error);
+                    showAlertDialog(context, "", "error");
+                  });
+                },
+              )),
               const SizedBox(
-                height: 10,
-              ),
-
-              SizedBox(
-                  child: InkWell(
-                    child: Container(
-                        padding: EdgeInsets.fromLTRB(2, 15, 2, 15),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: Colors.deepOrangeAccent,
-                        ),
-                        alignment: Alignment.center,
-                        child: const Text(
-                          '团长登录',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              letterSpacing: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white70),
-                        )),
-                    onTap: () {
-                      print(username.text);
-                      print(password.text);
-
-                      Map<String, dynamic> params = {
-                        "username": username.text,
-                        "password": password.text,
-                        "userType": 2
-                      };
-                      NetRequest()
-                          .request(MyApi.LOGIN, params: params)
-                          .then((response) async {
-                        if (response.code == 200) {
-                          LoginResponse model = LoginResponse.fromJson(response.model);
-                          final prefs = await SharedPreferences.getInstance();
-                          await prefs.setString('token', model.token!);
-                          await prefs.setString('userType', "2");
-
-                          Navigator.of(context).pushAndRemoveUntil(
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      ChangeNotifierProvider<BottomNaviProvider>(
-                                        create: (context) {
-                                          BottomNaviProvider provider =
-                                          BottomNaviProvider();
-                                          provider.changeBottomNaviIndex(0);
-                                          return provider;
-                                        },
-                                        child: Consumer<BottomNaviProvider>(
-                                            builder: (_, provider, __) {
-                                              return Container(child: IndexPage(userType: 2,));
-                                            }),
-                                      )),
-                                  (route) => false);
-                        } else {
-                          showAlertDialog(context, "登录失败", "");
-                        }
-                      }).catchError((error) {
-                        print(error);
-                        showAlertDialog(context, "", "error");
-                      });
-                    },
-                  )),
-              const SizedBox(
-                height: 10,
+                height: 20,
               ),
               SizedBox(
                   child: InkWell(
-                    child: Container(
-                        padding: EdgeInsets.fromLTRB(2, 15, 2, 15),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: Colors.deepOrangeAccent,
-                        ),
-                        alignment: Alignment.center,
-                        child: const Text(
-                          '注册',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              letterSpacing: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white70),
-                        )),
-                    onTap: () {
-                      Navigator.push(context,
-                          new MaterialPageRoute(
-                            builder: (BuildContext context) {
-                              return  RegisterPage();
-                            },
-                          ));
+                child: Container(
+                    padding: EdgeInsets.fromLTRB(2, 15, 2, 15),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.deepOrangeAccent,
+                    ),
+                    alignment: Alignment.center,
+                    child: const Text(
+                      '注册',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          letterSpacing: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white70),
+                    )),
+                onTap: () {
+                  Navigator.push(context, new MaterialPageRoute(
+                    builder: (BuildContext context) {
+                      return RegisterPage();
                     },
-                  )),
+                  ));
+                },
+              )),
             ],
           ),
         ),
